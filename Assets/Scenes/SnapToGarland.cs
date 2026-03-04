de1cdf3acd46f9fc48e05e9ed4394ceee61129c8
Srijani Prekki <sprek2@uic.edu>
2026-03-04 00:05:13 -0600

    Added movable paper people and garland snapping

---
+++ Assets/Scenes/SnapToGarland.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SnapToGarland : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SnapPoint"))
+        {
+            transform.position = other.transform.position;
+            transform.rotation = other.transform.rotation;
+
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+}
