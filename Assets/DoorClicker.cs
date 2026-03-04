00bbd4259e50edeac380bcd9c1d18cddb5b7321d
Zainab Ahmed <zainabahmed1204@gmail.com>
2026-03-03 20:25:46 -0600

    Door swinging open interaction

---
+++ Assets/DoorClicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DoorClicker : MonoBehaviour
+{
+    public float distance = 8f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                DoorToggle door = hit.collider.GetComponentInParent<DoorToggle>();
+
+                if (door != null)
+                {
+                    door.Toggle();
+                }
+            }
+        }
+    }
+}
