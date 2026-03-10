12a6569272f20f011e90dc640851656868620410
Kassandra Acosta Ramirez <acostakass@gmail.com>
2026-03-10 12:55:57 -0500

    Electricity room board setup and trigger structure

---
+++ Assets/Scripts/WireSnapZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WireSnapZone : MonoBehaviour
+{
+    public string correctWire;
+    public Transform snapPoint;
+    public ElectricityPuzzleManager manager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == correctWire)
+        {
+            other.transform.position = snapPoint.position;
+            other.transform.rotation = snapPoint.rotation;
+
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = true;
+            }
+
+            manager.CompleteWire(correctWire);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
