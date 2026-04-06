84af12305a1d7f25b8a64c8f48453df2864ba06c
Zainab Ahmed <zainabahmed1204@gmail.com>
2026-04-06 00:01:10 -0500

    update tablet room with tablet, terrain, scripts

---
+++ Assets/Scripts/HallwaySpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HallwaySpawnManager : MonoBehaviour
+{
+    public Transform playerObject;
+
+    void Start()
+    {
+        if (string.IsNullOrEmpty(SpawnState.nextHallwaySpawn))
+            return;
+
+        GameObject spawn = GameObject.Find(SpawnState.nextHallwaySpawn);
+
+        if (spawn != null && playerObject != null)
+        {
+            playerObject.position = spawn.transform.position;
+            playerObject.rotation = spawn.transform.rotation;
+        }
+
+        SpawnState.nextHallwaySpawn = "";
+    }
+}
